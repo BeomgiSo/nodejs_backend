520c6aee8909f3a9f92377987229ff6f4c1c9a95
RamG <ramg@Ramgui-M1-pro.local>
2022-03-17 20:17:56 +0900

    token활용한 인증기능 구현

--- apiRouter/index.js
+++ apiRouter/index.js
@@ -1,18 +1,24 @@
 const express =require("express");
 const router = express.Router();
-
 const User=require("../model/User");
-// 가입된 유저 리스트 (읽기) --Get으로 구현
+const loginRequired = require("../middleware/loginRequired");
 
-router.get("/user", async (req,res)=>{
-    try{
+// /api/user
+router.use(loginRequired);
+
+router.get("/user", async (req, res) => {
+  try {
     const allUser = await User.find({}).select("username");
     res.json(allUser);
-    }
-    catch(e){
-        res.json({status:"fail",message:e})
-    }
-})
+  } catch (e) {
+    res.json({ status: "fail", message: e });
+  }
+});
 
+router.get("/data", (req,res)=>{
+    res.json({
+        data:"123",
+    });
+});
 
 module.exports=router;
--- router/authRouter/index.js
+++ router/authRouter/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../model/User");
+const jwt =require("jsonwebtoken");
 
 
 router.post("/login", async (req,res)=>{
@@ -25,8 +26,19 @@ router.post("/login", async (req,res)=>{
       })
     }
     else{
+      const token = jwt.sign(
+        {
+        username : username
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn:"1h"
+      }
+      );
+
       res.json({
         status:"succ",
+        token,
       });
     }
   }
